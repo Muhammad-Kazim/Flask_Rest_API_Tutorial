6c82b0f3cbe270fd15dfe4b0d33f75e17d77e41f
Muhammad-Kazim <kazimsyed4@gmail.com>
2022-09-01 20:41:30 +0500

    database capability added along with update and methods changed accordingly

--- main.py
+++ main.py
@@ -1,44 +1,92 @@
 import argparse
 
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databsae.db'
+db = SQLAlchemy(app)
+
+
+class VideoModel(db.Model):
+    id = db.Column(db.INTEGER, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.INTEGER, nullable=False)
+    likes = db.Column(db.INTEGER, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name = {self.name}, views = {self.views}, likes = {self.likes})"
+
+
+# db.create_all()
 
 video_put_argparse = reqparse.RequestParser()
 video_put_argparse.add_argument("name", type=str, help="Name of the video is required", required=True)
 video_put_argparse.add_argument("views", type=int, help="Views of the video is required", required=True)
 video_put_argparse.add_argument("likes", type=int, help="Likes on the video is required", required=True)
 
-videos = {}
+video_patch_argparse = reqparse.RequestParser()
+video_patch_argparse.add_argument("name", type=str, help="Name of the video is required")
+video_patch_argparse.add_argument("views", type=int, help="Views of the video is required")
+video_patch_argparse.add_argument("likes", type=int, help="Likes on the video is required")
 
-
-def abort_if_video_id_doesnt_exist(video_id):
-    if video_id not in videos:
-        abort(404, message="Video does not exist...")
-
-
-def abort_if_video_id_exists(video_id):
-    if video_id in videos:
-        abort(409, message="Video does already exists...")
-
+resource_fields = {
+    'id' : fields.Integer,
+    'name' : fields.String,
+    'views' : fields.Integer,
+    'likes' : fields.String
+}
 
 class Video(Resource):
-    def get(self, video_id):
-        abort_if_video_id_doesnt_exist(video_id)
-        return videos[video_id]
 
+    @marshal_with(resource_fields)
+    def get(self, video_id):
+        result = VideoModel.query.filter_by(id=video_id).first_or_404(
+            description=f"Video with id={video_id} not found...")
+        return result, 200
+
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        abort_if_video_id_exists(video_id)
         args = video_put_argparse.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
+        result = VideoModel.query.filter_by(id=video_id).first()
+
+        if result:
+            abort(409, message="Video already exists...")
+
+        video = VideoModel(id=video_id, name=args['name'],
+                           views=args['views'], likes=args['likes'])
+
+        db.session.add(video)
+        db.session.commit()
+
+        return video, 201
+
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_patch_argparse.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first_or_404(
+            description=f"Video with id={video_id} not found...")
+
+        if args['name']:
+            result.name = args['name']
+        if args['views']:
+            result.views = args['views']
+        if args['likes']:
+            result.likes = args['likes']
+
+        db.session.commit()
+        return result
 
     def delete(self, video_id):
-        abort_if_video_id_doesnt_exist(video_id)
-        del videos[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first_or_404(
+            description=f"Video with id={video_id} not found...")
+
+        db.session.delete(result)
+        db.session.commit()
         return "", 204
 
 
--- test.py
+++ test.py
@@ -8,16 +8,29 @@ data = [
 
 BASE = "http://127.0.0.1:5000/"
 
+print('PUT REQUEST /n')
 for i, x in enumerate(data):
     response = requests.put(BASE + f"video/{i}", x)
     print(response.json())
 
 input()
+print('GET REQUEST /n')
+for i, x in enumerate(data):
+    response = requests.get(BASE + f"video/{i}", x)
+    print(response.json())
 
+
+input()
 response = requests.delete(BASE + f"video/{1}")
 print(response)
 
-input()
+print('GET REQUEST AFTER DELETE/n')
+for i, x in enumerate(data):
+    response = requests.get(BASE + f"video/{i}", x)
+    print(response.json())
 
-response = requests.get(BASE + f"video/{0}")
+input()
+print('PATCH REQUEST')
+
+response = requests.patch(BASE + f"video/{2}", {"name": "Zulfi"})
 print(response.json())
